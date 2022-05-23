ef6bb01093514a50b6431966f3dc5e90eef62a7a
Himarsha Jayanetti <53842088+himarshaj@users.noreply.github.com>
2022-05-23 00:21:51 -0400

    updated the output file location for the final csv file

--- source/temporal_analysis/temporal_analysis_memdate.sh
+++ source/temporal_analysis/temporal_analysis_memdate.sh
@@ -143,31 +143,31 @@ cat /data/LogAnalysis/access_pattern/temporal_analysis/after_s2_clean/output/rob
 
 * IA 2012
 
-cat /data/LogAnalysis/access_pattern/temporal_analysis/after_s2_clean/output/human_status_code/20120202.IA.Status200.memyearmonth| awk '{print "IA_2012_H_200,"$2","$1}' > memdate_out_IA2012.csv 
-cat /data/LogAnalysis/access_pattern/temporal_analysis/after_s2_clean/output/human_status_code/20120202.IA.Status404.memyearmonth| awk '{print "IA_2012_H_404,"$2","$1}' >> memdate_out_IA2012.csv  
-cat /data/LogAnalysis/access_pattern/temporal_analysis/after_s2_clean/output/human_status_code/20120202.IA.Status503.memyearmonth| awk '{print "IA_2012_H_503,"$2","$1}' >> memdate_out_IA2012.csv 
-cat /data/LogAnalysis/access_pattern/temporal_analysis/after_s2_clean/output/robo_status_code/20120202.IA.Status200.memyearmonth| awk '{print "IA_2012_R_200,"$2","$1}' >> memdate_out_IA2012.csv 
-cat /data/LogAnalysis/access_pattern/temporal_analysis/after_s2_clean/output/robo_status_code/20120202.IA.Status404.memyearmonth| awk '{print "IA_2012_R_404,"$2","$1}' >> memdate_out_IA2012.csv 
-cat /data/LogAnalysis/access_pattern/temporal_analysis/after_s2_clean/output/robo_status_code/20120202.IA.Status503.memyearmonth| awk '{print "IA_2012_R_503,"$2","$1}' >> memdate_out_IA2012.csv 
+cat /data/LogAnalysis/access_pattern/temporal_analysis/after_s2_clean/output/human_status_code/20120202.IA.Status200.memyearmonth| awk '{print "IA_2012_H_200,"$2","$1}' > /data/LogAnalysis/access_pattern/temporal_analysis/after_s2_clean/output/memdate_out_IA2012.csv 
+cat /data/LogAnalysis/access_pattern/temporal_analysis/after_s2_clean/output/human_status_code/20120202.IA.Status404.memyearmonth| awk '{print "IA_2012_H_404,"$2","$1}' >> /data/LogAnalysis/access_pattern/temporal_analysis/after_s2_clean/output/memdate_out_IA2012.csv  
+cat /data/LogAnalysis/access_pattern/temporal_analysis/after_s2_clean/output/human_status_code/20120202.IA.Status503.memyearmonth| awk '{print "IA_2012_H_503,"$2","$1}' >> /data/LogAnalysis/access_pattern/temporal_analysis/after_s2_clean/output/memdate_out_IA2012.csv 
+cat /data/LogAnalysis/access_pattern/temporal_analysis/after_s2_clean/output/robo_status_code/20120202.IA.Status200.memyearmonth| awk '{print "IA_2012_R_200,"$2","$1}' >> /data/LogAnalysis/access_pattern/temporal_analysis/after_s2_clean/output/memdate_out_IA2012.csv 
+cat /data/LogAnalysis/access_pattern/temporal_analysis/after_s2_clean/output/robo_status_code/20120202.IA.Status404.memyearmonth| awk '{print "IA_2012_R_404,"$2","$1}' >> /data/LogAnalysis/access_pattern/temporal_analysis/after_s2_clean/output/memdate_out_IA2012.csv 
+cat /data/LogAnalysis/access_pattern/temporal_analysis/after_s2_clean/output/robo_status_code/20120202.IA.Status503.memyearmonth| awk '{print "IA_2012_R_503,"$2","$1}' >> /data/LogAnalysis/access_pattern/temporal_analysis/after_s2_clean/output/memdate_out_IA2012.csv 
 
 
 * IA 2019
 
 
-cat /data/LogAnalysis/access_pattern/temporal_analysis/after_s2_clean/output/human_status_code/20190207.IA.Status200.memyearmonth| awk '{print "IA_2019_H_200,"$2","$1}' > memdate_out_IA2019.csv 
-cat /data/LogAnalysis/access_pattern/temporal_analysis/after_s2_clean/output/human_status_code/20190207.IA.Status404.memyearmonth| awk '{print "IA_2019_H_404,"$2","$1}' >> memdate_out_IA2019.csv  
-cat /data/LogAnalysis/access_pattern/temporal_analysis/after_s2_clean/output/human_status_code/20190207.IA.Status503.memyearmonth| awk '{print "IA_2019_H_503,"$2","$1}' >> memdate_out_IA2019.csv 
-cat /data/LogAnalysis/access_pattern/temporal_analysis/after_s2_clean/output/robo_status_code/20190207.IA.Status200.memyearmonth| awk '{print "IA_2019_R_200,"$2","$1}' >> memdate_out_IA2019.csv 
-cat /data/LogAnalysis/access_pattern/temporal_analysis/after_s2_clean/output/robo_status_code/20190207.IA.Status404.memyearmonth| awk '{print "IA_2019_R_404,"$2","$1}' >> memdate_out_IA2019.csv 
-cat /data/LogAnalysis/access_pattern/temporal_analysis/after_s2_clean/output/robo_status_code/20190207.IA.Status503.memyearmonth| awk '{print "IA_2019_R_503,"$2","$1}' >> memdate_out_IA2019.csv 
+cat /data/LogAnalysis/access_pattern/temporal_analysis/after_s2_clean/output/human_status_code/20190207.IA.Status200.memyearmonth| awk '{print "IA_2019_H_200,"$2","$1}' > /data/LogAnalysis/access_pattern/temporal_analysis/after_s2_clean/output/memdate_out_IA2019.csv 
+cat /data/LogAnalysis/access_pattern/temporal_analysis/after_s2_clean/output/human_status_code/20190207.IA.Status404.memyearmonth| awk '{print "IA_2019_H_404,"$2","$1}' >> /data/LogAnalysis/access_pattern/temporal_analysis/after_s2_clean/output/memdate_out_IA2019.csv  
+cat /data/LogAnalysis/access_pattern/temporal_analysis/after_s2_clean/output/human_status_code/20190207.IA.Status503.memyearmonth| awk '{print "IA_2019_H_503,"$2","$1}' >> /data/LogAnalysis/access_pattern/temporal_analysis/after_s2_clean/output/memdate_out_IA2019.csv 
+cat /data/LogAnalysis/access_pattern/temporal_analysis/after_s2_clean/output/robo_status_code/20190207.IA.Status200.memyearmonth| awk '{print "IA_2019_R_200,"$2","$1}' >> /data/LogAnalysis/access_pattern/temporal_analysis/after_s2_clean/output/memdate_out_IA2019.csv 
+cat /data/LogAnalysis/access_pattern/temporal_analysis/after_s2_clean/output/robo_status_code/20190207.IA.Status404.memyearmonth| awk '{print "IA_2019_R_404,"$2","$1}' >> /data/LogAnalysis/access_pattern/temporal_analysis/after_s2_clean/output/memdate_out_IA2019.csv 
+cat /data/LogAnalysis/access_pattern/temporal_analysis/after_s2_clean/output/robo_status_code/20190207.IA.Status503.memyearmonth| awk '{print "IA_2019_R_503,"$2","$1}' >> /data/LogAnalysis/access_pattern/temporal_analysis/after_s2_clean/output/memdate_out_IA2019.csv 
 
 
 * PT 2019
 
 
-cat /data/LogAnalysis/access_pattern/temporal_analysis/after_s2_clean/output/human_status_code/20190207.PT.Status200.memyearmonth| awk '{print "PT_2019_H_200,"$2","$1}' > memdate_out_PT2019.csv 
-cat /data/LogAnalysis/access_pattern/temporal_analysis/after_s2_clean/output/human_status_code/20190207.PT.Status404.memyearmonth| awk '{print "PT_2019_H_404,"$2","$1}' >> memdate_out_PT2019.csv  
-cat /data/LogAnalysis/access_pattern/temporal_analysis/after_s2_clean/output/human_status_code/20190207.PT.Status503.memyearmonth| awk '{print "PT_2019_H_503,"$2","$1}' >> memdate_out_PT2019.csv 
-cat /data/LogAnalysis/access_pattern/temporal_analysis/after_s2_clean/output/robo_status_code/20190207.PT.Status200.memyearmonth| awk '{print "PT_2019_R_200,"$2","$1}' >> memdate_out_PT2019.csv 
-cat /data/LogAnalysis/access_pattern/temporal_analysis/after_s2_clean/output/robo_status_code/20190207.PT.Status404.memyearmonth| awk '{print "PT_2019_R_404,"$2","$1}' >> memdate_out_PT2019.csv 
-cat /data/LogAnalysis/access_pattern/temporal_analysis/after_s2_clean/output/robo_status_code/20190207.PT.Status503.memyearmonth| awk '{print "PT_2019_R_503,"$2","$1}' >> memdate_out_PT2019.csv 
+cat /data/LogAnalysis/access_pattern/temporal_analysis/after_s2_clean/output/human_status_code/20190207.PT.Status200.memyearmonth| awk '{print "PT_2019_H_200,"$2","$1}' > /data/LogAnalysis/access_pattern/temporal_analysis/after_s2_clean/output/memdate_out_PT2019.csv 
+cat /data/LogAnalysis/access_pattern/temporal_analysis/after_s2_clean/output/human_status_code/20190207.PT.Status404.memyearmonth| awk '{print "PT_2019_H_404,"$2","$1}' >> /data/LogAnalysis/access_pattern/temporal_analysis/after_s2_clean/output/memdate_out_PT2019.csv  
+cat /data/LogAnalysis/access_pattern/temporal_analysis/after_s2_clean/output/human_status_code/20190207.PT.Status503.memyearmonth| awk '{print "PT_2019_H_503,"$2","$1}' >> /data/LogAnalysis/access_pattern/temporal_analysis/after_s2_clean/output/memdate_out_PT2019.csv 
+cat /data/LogAnalysis/access_pattern/temporal_analysis/after_s2_clean/output/robo_status_code/20190207.PT.Status200.memyearmonth| awk '{print "PT_2019_R_200,"$2","$1}' >> /data/LogAnalysis/access_pattern/temporal_analysis/after_s2_clean/output/memdate_out_PT2019.csv 
+cat /data/LogAnalysis/access_pattern/temporal_analysis/after_s2_clean/output/robo_status_code/20190207.PT.Status404.memyearmonth| awk '{print "PT_2019_R_404,"$2","$1}' >> /data/LogAnalysis/access_pattern/temporal_analysis/after_s2_clean/output/memdate_out_PT2019.csv 
+cat /data/LogAnalysis/access_pattern/temporal_analysis/after_s2_clean/output/robo_status_code/20190207.PT.Status503.memyearmonth| awk '{print "PT_2019_R_503,"$2","$1}' >> /data/LogAnalysis/access_pattern/temporal_analysis/after_s2_clean/output/memdate_out_PT2019.csv 
